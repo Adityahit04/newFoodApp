3b3bffbe88882ee986585879c9fb5953e393633c
Adityahit <sadityathakur01@gmail.com>
2024-04-10 01:15:38 +0530

    router path updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import StoreContextProvider from './Context/StoreContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // Corrected import statement
+import App from "./App.jsx";
+import "./index.css";
+import StoreContextProvider from "./Context/StoreContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
